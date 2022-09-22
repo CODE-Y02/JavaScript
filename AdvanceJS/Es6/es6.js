c0452edabd8f1d95a8b50c7e2c2074065bef3973
yatharth <lakhatey@gmail.com>
2022-09-22 17:50:48 +0530

    concatinate using ...

--- AdvanceJS/Es6/es6.js
+++ AdvanceJS/Es6/es6.js
@@ -13,15 +13,18 @@
 
 // rest operator and spread combo
 
-let sumAll = (a,b,...args) => {
-  let mul = a*b;
-  console.log(args)
+let sumAll = (a, b, ...args) => {
+  let mul = a * b;
+  console.log(args);
   let sum = 0;
   for (const arg of args) {
     sum += arg;
   }
-  return [sum,mul];
+  return [sum, mul];
 };
 console.log(sumAll(1, 2, 3, 9, 55, 5, 5));
 
-
+let a = [1, 5, 7];
+let b = [7, "a"];
+let newarr = [...a, ...b];
+console.log(newarr);
