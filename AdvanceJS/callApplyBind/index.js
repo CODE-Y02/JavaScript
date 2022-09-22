6016e386d1643635f2b64331257ab947f936cc61
yatharth <lakhatey@gmail.com>
2022-09-22 15:32:40 +0530

    doing task

--- AdvanceJS/callApplyBind/index.js
+++ AdvanceJS/callApplyBind/index.js
@@ -18,3 +18,58 @@ bound(arg1, arg2, arg3);
 */
 
 //  NOTE :- THIS MAY CREATE PLOBLEM WITH ARROW FUNCTION
+
+// TASKS
+// Write a function and use 'call' to call the function
+
+let sum = function (a, b) {
+  console.log(a + b);
+};
+sum.call(this, 1, 2);
+
+//Write a program using apply
+sum.apply(this, [11, 2]);
+
+//Write a program using bind
+let newFun = sum.bind(this);
+newFun(5000, 2);
+
+/*
+Create a new object called Student with age 20 , write a function which prints the age of the student from the student object.
+Dont pass in args. Read from 'this' and use BIND
+*/
+
+let student = {
+  age: 20,
+};
+
+function print() {
+  console.log(`age = ${this.age}`);
+}
+
+let printAge = print.bind(student);
+printAge();
+
+
+//Understand currying
+let multiply = function(x,y){
+    console.log(x*y);
+}
+
+let multiplyByTwo = multiply.bind(this,2) 
+/*
+in multiplyByTwo we are setting x value permanently as x =2;
+*/
+multiplyByTwo(5)  // 50== y in function
+
+
+let multiplyByThree = multiply.bind(this,3) 
+multiplyByThree(5) 
+
+
+
+
+
+
+
+
