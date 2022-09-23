746f9b5cc68b574fc30ae9f5f8967913f5b5d1f3
yatharth <lakhatey@gmail.com>
2022-09-23 22:47:01 +0530

    eventlistnere along with closure

--- AdvanceJS/CallBack_EventLoop/callBackFun.js
+++ AdvanceJS/CallBack_EventLoop/callBackFun.js
@@ -1,7 +1,7 @@
 //what is callback fun ?
 // function passed inside another function is called callback function;
 // it makes async js possible
-
+/*
 setTimeout(function () {
   console.log('time');
 }, 5000);
@@ -14,3 +14,20 @@ function x(y) {
 x(function y() {
   console.log("y");
 });
+*/
+
+
+//counting how many time btn is clicked 
+
+// eventlistnere along with closure 
+function attachEventListber(){
+    let count =0;
+    document.getElementById("clickMe").addEventListener("click", function xyz() {
+        console.log("button clicked",++count);
+        
+      });
+}
+attachEventListber();
+
+
+
