4fbb55165e7a56f8fd15f23fbaeb7882c8a79525
yatharth <lakhatey@gmail.com>
2022-09-23 14:15:08 +0530

    Function in JS :- fun statement , fun expression

---
+++ AdvanceJS/Functions_JS/fun.js
@@ -0,0 +1,9 @@
+// function statement aka function declaration
+function a() {
+  console.log("a is called");
+}
+
+//function expression :- when function is assigned to variable
+var b = function () {
+  console.log("b is called");
+};
