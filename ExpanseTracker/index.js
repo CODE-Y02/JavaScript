95cefaf591536a0b1226ee95ea658f59d85cb313
yatharth <lakhatey@gmail.com>
2022-09-25 10:34:12 +0530

    Expense tracked modefied ver 2

--- ExpanseTracker/index.js
+++ ExpanseTracker/index.js
@@ -3,18 +3,20 @@
 let form = document.getElementById("form");
 
 //on dom loaded
-window.addEventListener("DOMContentLoaded", showOnScreen);
+window.addEventListener("DOMContentLoaded", showOnScreen());
 
 // console.dir(form);
 form.addEventListener("submit", addExpense);
 
-function addExpense() {
+function addExpense(e) {
+  e.preventDefault();
   let amount = document.getElementById("expanse");
   let desc = document.getElementById("desc");
   let type = document.getElementById("ExpanaseType");
 
   //new expense obj
   let expenseObj = {
+    id: Date.now(),
     amount: amount.value,
     desc: desc.value,
     type: type.value,
@@ -22,18 +24,18 @@ function addExpense() {
 
   //get local storage
   // if AllExpenses =0 ; then set empty array
-  let storeArr = JSON.parse(localStorage.getItem("AllExpenses")) || [];
+  let AllExpenses = JSON.parse(localStorage.getItem("AllExpenses")) || [];
 
   // console.log(storeArr);
 
   //add new expense
-  storeArr.push(expenseObj);
+  AllExpenses.push(expenseObj);
 
   //saving on local storage
-  localStorage.setItem("AllExpenses", JSON.stringify(storeArr));
+  localStorage.setItem("AllExpenses", JSON.stringify(AllExpenses));
 
   //show on screen
-  showOnScreen();
+  showOnScreen(AllExpenses);
 
   //clear values
   amount.value = "";
@@ -41,53 +43,43 @@ function addExpense() {
   type.value = "other";
 }
 
-function showOnScreen() {
-  let list = document.getElementById("listExpense");
+function showOnScreen(expenseArr = null) {
+  if (!expenseArr) {
+    expenseArr = JSON.parse(localStorage.getItem("AllExpenses")) || [];
+  }
+  const list = document.getElementById("listExpense");
   list.innerHTML = "";
-  // all expenses array
-  let AllExpenses = JSON.parse(localStorage.getItem("AllExpenses")) || [];
+  // console.log(expenseArr);
+  expenseArr.forEach((expense) => {
+    let newHtml = ` <li> Expense: ${expense.type}     Amount: ${
+      expense.amount
+    }    Description:${expense.desc} <button onclick="deleteEditExp('${
+      expense.id
+    }')">Delete</button><button onclick="deleteEditExp('${
+      expense.id
+    }',${true})">Edit</button></li>`;
 
-  // display on dom
-  AllExpenses.forEach((expanse, idx) => {
-    let newHtml = `<li class="listItem" id="${idx}">Expense Type = ${expanse.type}   Amount =  ${expanse.amount}   Description = ${expanse.desc} <button onclick='delExp("${idx}")'>Delete</button><button onclick="editExp('${idx}')">Edit</button></li>`;
-
-    //create new li
     list.innerHTML = list.innerHTML + newHtml;
   });
 }
 
-function delExp(idx) {
-  console.log(idx);
-
-  // all expenses array
+function deleteEditExp(id, edit = false) {
   let AllExpenses = JSON.parse(localStorage.getItem("AllExpenses"));
+  // console.log("ALL ", AllExpenses);
+  let filteredArr = AllExpenses.filter((expense) => expense.id != id);
+  // console.log("after = ", filteredArr);
+  // console.log(edit);
 
-  let newArr = AllExpenses.filter((curr, index) => {
-    if (index != idx) return curr;
-  });
+  if (edit) {
+    //find expense
+    let expenseArr = AllExpenses.filter((expense) => (expense.id = id));
+
+    document.getElementById("expanse").value = expenseArr[0].amount;
+    document.getElementById("desc").value = expenseArr[0].desc;
+    document.getElementById("ExpanaseType").value = expenseArr[0].type;
+  }
 
   //saving on local storage
-  localStorage.setItem("AllExpenses", JSON.stringify(newArr));
-  showOnScreen();
-}
-
-function editExp(id) {
-  // all expenses array
-  let AllExpenses = JSON.parse(localStorage.getItem("AllExpenses"));
-
-  let expense = AllExpenses[id]; // id --> index
-
-  //now delete expense
-  delExp(id);
-
-  showOnScreen();
-
-  //set value of form
-  let amount = document.getElementById("expanse");
-  let desc = document.getElementById("desc");
-  let type = document.getElementById("ExpanaseType");
-
-  amount.value = expense.amount;
-  desc.value = expense.desc;
-  type.value = expense.type;
+  localStorage.setItem("AllExpenses", JSON.stringify(filteredArr));
+  showOnScreen(filteredArr);
 }
