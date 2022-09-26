21d3d2cb661c2181e05a70ed5f7e400610606610
yatharth <lakhatey@gmail.com>
2022-09-26 18:20:08 +0530

    !!!! PROMISES 2 CREATE 4th post REMAINING

--- AsyncJs/Promises.js
+++ AsyncJs/Promises.js
@@ -2,19 +2,22 @@ const posts = [
   {
     title: "Post One",
     body: "this is post one",
+    createdAt: new Date().getTime(),
   },
   {
     title: "Post two",
     body: "this is post two",
+    createdAt: new Date().getTime(),
   },
 ];
-
 function getPost() {
   setTimeout(() => {
     let output = "";
 
     posts.forEach((post, index) => {
-      output += `<li>${post.title}</li>`;
+      output += `<li>${post.title} Last update ${Math.floor(
+        (new Date().getTime() - post.createdAt) / 1000
+      )} sec before</li>`;
     });
     document.body.innerHTML = output;
   }, 1000);
@@ -23,7 +26,7 @@ function getPost() {
 function createPost(post) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      posts.push(post);
+      posts.push({ ...post, createdAt: new Date().getTime() });
 
       const error = false;
       // const error = true;
@@ -36,9 +39,55 @@ function createPost(post) {
   });
 }
 
-createPost({ title: "Post three", body: "this is post three" }).then(getPost).catch(err => console.log(err))
+// createPost({ title: "Post three", body: "this is post three" })
+//   .then(getPost)
+//   .catch((err) => console.log(err));
+
+// 2)Create a new function called delete post which uses promises and deletes in 1 second (processing time - mimic it with setimeout).
+
+function deletePost() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (posts.length) {
+        resolve(posts.pop());
+      } else {
+        reject("NO POSTS : Array is empty now");
+      }
+    }, 1000);
+  });
+}
+
+createPost({ title: "Post three", body: "this is post three" })
+  .then(() => {
+    getPost();
+    deletePost().then(() => {
+      //delete 1
+      getPost();
+      deletePost().then(() => {
+        //delete 2
+        getPost();
+        deletePost().then(() => {
+          //delete 3
+          getPost();
+          deletePost()
+            .then(() => {
+              //gives error
+              getPost();
+            })
+            .catch((err) => console.log(err));
+        });
+      });
+    });
+  })
+  .catch((err) => console.log(err));
 
 
-//Promise.all
 
-  
+createPost({ title: "Post FOUR", body: "this is post FOUR" }).then(()=>{
+  getPost();
+  console.log("post 4 created")
+  deletePost().then(()=>{
+    getPost();
+    console.log('post 4 del')
+  }).catch(err=>console.log(err))
+}).catch(err=>console.log(err))
