3064eb43f842085bf44c1e2cab647d735f3bd5d7
yatharth <lakhatey@gmail.com>
2022-09-26 12:10:38 +0530

    create a promise as per the youtuber does

--- AsyncJs/Promises.js
+++ AsyncJs/Promises.js
@@ -1,28 +1,44 @@
 const posts = [
-    {
-      title: "Post One",
-      body: "this is post one",
-    },
-    {
-      title: "Post two",
-      body: "this is post two",
-    },
-  ];
-  
-  function getPost() {
-    setTimeout(() => {
-      let output = "";
-  
-      posts.forEach((post, index) => {
-        output += `<li>${post.title}</li>`;
-      });
-      document.body.innerHTML = output;
-    }, 1000);
-  }
-  
-  function createPost(post, callback) {
+  {
+    title: "Post One",
+    body: "this is post one",
+  },
+  {
+    title: "Post two",
+    body: "this is post two",
+  },
+];
+
+function getPost() {
+  setTimeout(() => {
+    let output = "";
+
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPost(post) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       posts.push(post);
-      callback();
+
+      const error = false;
+      // const error = true;
+      if (!error) {
+        resolve();
+      } else {
+        reject("ERROR : Something is wrong");
+      }
     }, 2000);
-  }
+  });
+}
+
+createPost({ title: "Post three", body: "this is post three" }).then(getPost).catch(err => console.log(err))
+
+
+//Promise.all
+
+  
