3730f7a4d7e5742a01cb30f58435d2f474d1ed1d
yatharth <lakhatey@gmail.com>
2022-09-28 11:34:23 +0530

    AAA 2

--- AsyncJs/AsyncAwait.js
+++ AsyncJs/AsyncAwait.js
@@ -34,12 +34,42 @@ console.log("person4: shows ticket");
 console.log("person5: shows ticket");
 */
 
-const preMovie = async () => "premovie";
+const preMovie = async () => {
+  const promiseWifeBringingTicks = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("ticket");
+    }, 3000);
+  });
+
+  const getPopcorn = new Promise((resolve, reject) => resolve(`popcorn`));
+
+  const addButter = new Promise((resolve, reject) => resolve(`butter`));
+
+  let ticket = await promiseWifeBringingTicks;
+  //   console.log("tick =", ticket);  //--> ticket contain resolved val
+
+  console.log(`wife: i have ${ticket}`);
+  console.log(`husband: we should go in`);
+  console.log(`wife: no i am hungry`);
+
+  let popcorn = await getPopcorn;
+  console.log(`husband: i got some ${popcorn}`);
+  console.log(`husband: we should go in`);
+  console.log(`wife: no i need butter on my popcorn`);
+
+  let butter = await addButter;
+  console.log(`husband: i got some ${butter}`);
+  console.log(`husband: anything else ?`);
+  console.log(`wife: lets go we are getting late`);
+  console.log(`husband: Thanks for remainder **grins**`);
+
+  return ticket;
+};
 
 // console.log(preMovie());
 
 // async function always return promise
-preMovie().then((msg) => console.log(msg));
+preMovie().then((msg) => console.log(`Person3: shows ${msg}`));
 
 console.log("person4: shows ticket");
 console.log("person5: shows ticket");
