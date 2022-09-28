b290e9f6ab3c870f0f6ac1893d1b741368682896
yatharth <lakhatey@gmail.com>
2022-09-28 11:05:58 +0530

    AA p 2

--- AsyncJs/AsyncAwait.js
+++ AsyncJs/AsyncAwait.js
@@ -9,29 +9,20 @@ const promiseWifeBringingTicks = new Promise((resolve, reject) => {
   }, 3000);
 });
 
-promiseWifeBringingTicks.then((t) => {
-  console.log(`person3: shows ${t}`);
+// promiseWifeBringingTicks.then((t) => {
+//   console.log(`person3: shows ${t}`);
+// });
+
+const getPopcorn = promiseWifeBringingTicks.then((t) => {
+  console.log(`husband: we should go in`);
+  console.log(`wife: no i am hungry`);
+  return new Promise((resolve, reject) => resolve(`${t} popcorn`));
 });
+
+
+getPopcorn.then((t)=>console.log(t))
+
+
 
 console.log("person4: shows ticket");
 console.log("person5: shows ticket");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
