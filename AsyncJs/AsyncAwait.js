aa14865408617b1e4bc4a512cfea309dd0fe1420
yatharth <lakhatey@gmail.com>
2022-09-28 11:16:49 +0530

    AAA 1

--- AsyncJs/AsyncAwait.js
+++ AsyncJs/AsyncAwait.js
@@ -2,7 +2,7 @@
 
 console.log("person1: shows ticket");
 console.log("person2: shows ticket");
-
+/*
 const promiseWifeBringingTicks = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve("ticket");
@@ -14,15 +14,32 @@ const promiseWifeBringingTicks = new Promise((resolve, reject) => {
 // });
 
 const getPopcorn = promiseWifeBringingTicks.then((t) => {
+    console.log(`wife: i have tickets`);
   console.log(`husband: we should go in`);
   console.log(`wife: no i am hungry`);
   return new Promise((resolve, reject) => resolve(`${t} popcorn`));
 });
 
+const getButter = getPopcorn.then((t) => {
+    console.log(`husband: i got some popcorn`);
+  console.log(`husband: we should go in`);
+  console.log(`wife: no i need butter on my popcorn`);
 
-getPopcorn.then((t)=>console.log(t))
+  return new Promise((resolve, reject) => resolve(`${t} butter`));
+});
 
+getButter.then((t) => console.log(t));
 
+console.log("person4: shows ticket");
+console.log("person5: shows ticket");
+*/
+
+const preMovie = async () => "premovie";
+
+// console.log(preMovie());
+
+// async function always return promise
+preMovie().then((msg) => console.log(msg));
 
 console.log("person4: shows ticket");
 console.log("person5: shows ticket");
