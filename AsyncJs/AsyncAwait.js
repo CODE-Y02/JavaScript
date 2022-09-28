232421b98b7eb693ee80fec05951122ac0b3d84b
yatharth <lakhatey@gmail.com>
2022-09-28 10:58:12 +0530

    AA promise 1

---
+++ AsyncJs/AsyncAwait.js
@@ -0,0 +1,37 @@
+//  when there async you need to wait
+
+console.log("person1: shows ticket");
+console.log("person2: shows ticket");
+
+const promiseWifeBringingTicks = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("ticket");
+  }, 3000);
+});
+
+promiseWifeBringingTicks.then((t) => {
+  console.log(`person3: shows ${t}`);
+});
+
+console.log("person4: shows ticket");
+console.log("person5: shows ticket");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
